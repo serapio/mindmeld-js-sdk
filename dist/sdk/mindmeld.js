b383db1b526d7a80e7292ab78dfb05c6a2cbeea6
J.J. Jackson <jj@expectlabs.com>
2014-08-01 17:30:29 -0700

    [VN] Use 600 as the breakpoint for mobile layouts

--- dist/sdk/mindmeld.js
+++ dist/sdk/mindmeld.js
@@ -2540,11 +2540,12 @@ var Faye = (function() {
     return Faye;
 
 })();
-/**
- * Adapted from https://github.com/ForbesLindesay/ajax
- * Removed the require/module code.
- */
+;(function () {
 
+
+/**
+ * Inlining type fn
+ */
 var type = function (val) {
   switch (toString.call(val)) {
     case '[object Function]': return 'function';
@@ -2572,6 +2573,75 @@ var type = function (val) {
 
   return typeof val;
 };
+
+/**
+ * jQuery's serialization fns, adapted
+ * ajax's were buggy, so going back to the source.
+ */
+
+var r20 = /%20/g,
+	rbracket = /\[\]$/;
+
+function buildParams( prefix, obj, add ) {
+	var name;
+
+	if ( type(obj) === 'array' ) {
+		// Serialize array item.
+		obj.forEach(function( v, i ) {
+			if ( rbracket.test( prefix ) ) {
+				// Treat each array item as a scalar.
+				add( prefix, v );
+
+			} else {
+				// Item is non-scalar (array or object), encode its numeric index.
+				buildParams(
+					prefix + '[' + ( typeof v === 'object' ? i : '' ) + ']',
+					v,
+					add
+				);
+			}
+		});
+
+	} else if ( type( obj ) === 'object' ) {
+		// Serialize object item.
+		for ( name in obj ) {
+			buildParams( prefix + '[' + name + ']', obj[ name ], add );
+		}
+
+	} else {
+		// Serialize scalar item.
+		add( prefix, obj );
+	}
+}
+
+// Serialize an array of form elements or a set of
+// key/values into a query string
+var param = function( a ) {
+	var prefix,
+		s = [],
+		add = function( key, value ) {
+			// If value is a function, invoke it and return its value
+      // XXX: We want the double equals here, since that's what jQuery used.
+			value = type(value) === 'function' ? value() : ( value == null ? '' : value );
+			s[ s.length ] = encodeURIComponent( key ) + '=' + encodeURIComponent( value );
+		};
+
+	for ( prefix in a ) {
+		buildParams( prefix, a[ prefix ], add );
+	}
+
+	// Return the resulting serialization
+	return s.join( '&' ).replace( r20, '+' );
+};
+
+
+
+
+/**
+ * Adapted from https://github.com/ForbesLindesay/ajax
+ * Removed the require/module code.
+ */
+
 
 var jsonpID = 0,
     document = window.document,
@@ -2824,28 +2894,6 @@ ajax.getJSON = function(url, success){
 
 var escape = encodeURIComponent
 
-function serialize(params, obj, traditional, scope){
-  var array = type(obj) === 'array';
-  for (var key in obj) {
-    var value = obj[key];
-
-    if (scope) key = traditional ? scope : scope + '[' + (array ? '' : key) + ']'
-    // handle data in serializeArray() format
-    if (!scope && array) params.add(value.name, value.value)
-    // recurse into nested objects
-    else if (traditional ? (type(value) === 'array') : (type(value) === 'object'))
-      serialize(params, value, traditional, key)
-    else params.add(key, value)
-  }
-}
-
-function param(obj, traditional){
-  var params = []
-  params.add = function(k, v){ this.push(escape(k) + '=' + escape(v)) }
-  serialize(params, obj, traditional)
-  return params.join('&').replace('%20', '+')
-}
-
 function extend(target) {
   var slice = Array.prototype.slice;
   slice.call(arguments, 1).forEach(function(source) {
@@ -2855,6 +2903,10 @@ function extend(target) {
   })
   return target
 }
+
+window.ajax = ajax;
+
+})();
 
 /* global window, ajax, Faye */
 
@@ -2868,34 +2920,107 @@ var MM = ( function (window, ajax, Faye) {
 
     var MM = window.MM = window.MM || {};
 
-    var _extend = function (base) {
-      base = base || {};
+    var _isFunction = function(f) {
+        return 'function' === typeof f;
+    };
 
-      for (var i = 1; i < arguments.length; i++) {
-        var obj = arguments[i];
+    var _isWindow = function( obj ) {
+        return obj != null && obj === obj.window;
+    };
 
-        if (!obj) continue;
-
-        for (var key in obj) {
-          if (obj.hasOwnProperty(key)) {
-            base[key] = obj[key];
-          }
+    var _isPlainObject = function( obj ) {
+        // Not plain objects:
+        // - Any object or value whose internal [[Class]] property is not "[object Object]"
+        // - DOM nodes
+        // - window
+        if ( typeof obj !== "object" || obj.nodeType || _isWindow( obj ) ) {
+            return false;
         }
-      }
-      return base;
+
+        if ( obj.constructor &&
+            !hasOwn.call( obj.constructor.prototype, "isPrototypeOf" ) ) {
+            return false;
+        }
+
+        // If the function hasn't returned already, we're confident that
+        // |obj| is a plain object, created by {} or constructed with new Object
+        return true;
+    };
+
+    var _extend = function() {
+        var src, copyIsArray, copy, name, options, clone,
+            target = arguments[0] || {},
+            i = 1,
+            length = arguments.length,
+            deep = false;
+
+        // Handle a deep copy situation
+        if ( typeof target === "boolean" ) {
+            deep = target;
+
+            // skip the boolean and the target
+            target = arguments[ i ] || {};
+            i++;
+        }
+
+        // Handle case when target is a string or something (possible in deep copy)
+        if ( typeof target !== "object" && ! _isFunction(target) ) {
+            target = {};
+        }
+
+        // extend jQuery itself if only one argument is passed
+        if ( i === length ) {
+            target = this;
+            i--;
+        }
+
+        for ( ; i < length; i++ ) {
+            // Only deal with non-null/undefined values
+            if ( (options = arguments[ i ]) != null ) {
+                // Extend the base object
+                for ( name in options ) {
+                    src = target[ name ];
+                    copy = options[ name ];
+
+                    // Prevent never-ending loop
+                    if ( target === copy ) {
+                        continue;
+                    }
+
+                    // Recurse if we're merging plain objects or arrays
+                    if ( deep && copy && ( _isPlainObject(copy) || (copyIsArray = Array.isArray(copy)) ) ) {
+                        if ( copyIsArray ) {
+                            copyIsArray = false;
+                            clone = src && jQuery.isArray(src) ? src : [];
+
+                        } else {
+                            clone = src && _isPlainObject(src) ? src : {};
+                        }
+
+                        // Never move original objects, clone them
+                        target[ name ] = _extend( deep, clone, copy );
+
+                        // Don't bring in undefined values
+                    } else if ( copy !== undefined ) {
+                        target[ name ] = copy;
+                    }
+                }
+            }
+        }
+
+        // Return the modified object
+        return target;
     };
 
     var _isEmptyObject = function (obj) {
   		var name;
-  		for ( name in obj ) {
+  		for (name in obj ) {
   			return false;
   		}
   		return true;
   	};
 
-    var _isFunction = function(f) {
-      return 'function' === typeof f;
-    };
+
 
     var _onDocumentReady = function(f) {
       if (window.document.readyState === 'complete' ||
@@ -2920,7 +3045,7 @@ var MM = ( function (window, ajax, Faye) {
      * @private
      */
     Object.defineProperty(MM, 'version', {
-        value: '2.4.1',
+        value: '2.5.2',
         writable: false
     });
 
