0f2eddf6c0d33726e27cd964ad59b7e5987bc0b0
swaraj <swaraj@expectlabs.com>
2014-09-03 23:50:34 -0700

    starting initialize function, added selector for newly created element's dom container

--- src/widgets/microphone/microphone.js
+++ src/widgets/microphone/microphone.js
@@ -5,20 +5,32 @@
     var componentDocument = currentScript.ownerDocument;
 
     var mindmeldMicrophone = Object.create(HTMLElement.prototype);
+    var containerElement;
 
     mindmeldMicrophone.createdCallback = function () {
         var template = componentDocument.querySelector('#mindmeld-microphone');
         var clone = template.content.cloneNode(true);
         var shadow = this.createShadowRoot();
         shadow.appendChild(clone);
+        containerElement = shadow.querySelector('.mindmeld-microphone');
     };
 
-    mindmeldMicrophone.foo = function () {
-        console.log('called foo');
+    mindmeldMicrophone.initialize = function initialize () {
+        if (! MM.support.speechRecognition) {
+            console.log('This browser does not support speech recognition');
+            containerElement.classList.add('disabled');
+            return;
+        }
+
+        var listenerConfig = {
+
+        };
     };
 
     importerDocument.registerElement('mindmeld-microphone', {
         prototype: mindmeldMicrophone
     });
+
+
 
 }(MM));
