e290a433681a2a8ba54d8b23c7eb8e1eb5ab2b59
swaraj <swaraj@expectlabs.com>
2014-09-04 18:08:16 -0700

    added mic component to default gulp taskst

--- gulpfile.js
+++ gulpfile.js
@@ -52,8 +52,8 @@ require('./gulp/tasks/tests');
 
 
 // General Tasks
-gulp.task('archive', ['sdk.archive', 'docs', 'sw.build', 'vn.build', 'mic.build']);
-gulp.task('build', ['sdk.build', 'sw.build', 'vn.build', 'mic.build', 'tests.build']);
+gulp.task('archive', ['sdk.archive', 'docs', 'sw.build', 'vn.build', 'mic.build', 'micComponent.build']);
+gulp.task('build', ['sdk.build', 'sw.build', 'vn.build', 'mic.build', 'micComponent.build', 'tests.build']);
 
 gulp.task('docs', function () {
   // We do it this slightly hacky way, because you need to jsdoc all the files at once,
