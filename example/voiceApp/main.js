e70baeaea665138e3ca3f519d3f0b27a2b66bcb1
James Gill <jgill@expectlabs.com>
2014-09-25 16:55:32 -0700

    Re-arranging order of voiceApp/main.js.

--- example/voiceApp/main.js
+++ example/voiceApp/main.js
@@ -1,20 +1,113 @@
 /* global MM, $ */
 
-/* Initialize Widgets */
-var Cards = window.MindMeldCards;
-Cards.initialize({
-  parentSelector: '#cards',
-  cardSelector: '.card',
-  templateDirectory: '../../dist/widgets/cards',
-  templateName: 'cardTemplate',
-  animationDuration: 600
+/* Initialize MindMeld */
+
+var config = {
+  // The id of the MindMeld app with the crawled documents
+  appid: 'b7653579dda418eded308a4e491595057f337448'
+};
+
+MM.start(config, function onSuccess () {
+  console.log('MindMeld initialized successfully.');
+}, function onFail (error) {
+  console.error('MindMeld failed to initialize:', error);
 });
 
+/* Initialize Widgets */
+var Cards = window.MindMeldCards;
 var SearchInput = window.MindMeldSearchInput;
-
 var Microphone = window.MindMeldMicrophone;
 
+$(function () {
+  Cards.initialize({
+    parentSelector: '#cards',
+    cardSelector: '.card',
+    templateDirectory: '../../dist/widgets/cards',
+    templateName: 'cardTemplate',
+    animationDuration: 600
+  });
+
+  // Pass in DOM (not jQuery) elements to these initializers.
+  Microphone.initialize($('.mindmeld-microphone')[0]);
+
+  SearchInput.initialize($('.mindmeld-search')[0]);
+
+});
+
+/* Set up widget events */
+
+Microphone.on('start', function () {
+  console.log('Microphone started');
+});
+
+Microphone.on('end', function () {
+  console.log('Microphone stopped');
+});
+
+/*
+ * The error event has a String `error` field.  This is a short description
+ * of the problem.  For a full list, please see
+ * https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
+ */
+Microphone.on('error', function (event) {
+  // Some errors are benign
+  if (event.error === 'aborted') {
+    console.log('Microphone aborted');
+  } else if (event.error === 'no-speech') {
+    console.log('Microphone did not receive any speech.');
+  } else {
+    console.error('Microphone error', event.error);
+  }
+});
+
+/*
+ * This is the event handler when the microphone gets a result.  The result
+ * object has a String `transcript` field, which contains the text of the
+ * result, and a Boolean `final` field, which specifies whether the result is
+ * the final result for this speech segment.  There will be many
+ * `final == false` results, of increasing result (and accuracy).
+ *
+ * Here, we show the interim (`final == false`) results in the search field,
+ * but greyed out to signify they are not final.  The final results are in
+ * black.  We only submit the final results to MindMeld, since there are
+ * many interim results that have minimal signal value.
+ */
+Microphone.on('result', function(result) {
+  SearchInput.setText(result.transcript, result.final);
+  if (result.final) {
+    submitText(result.transcript);
+  }
+});
+
+/*
+ * This is fired when the SearchInput is 'submitted', which means either
+ * the magnifying glass search button is pressed, or the user presses
+ * `<Return>` in the text box.
+ */
+SearchInput.on('submitText', function (text) {
+  submitText(text);
+});
+
 /* Helper functions for processing and submitting data */
+
+/*
+ * This submits the text (aka `textEntry`) to the MindMeld API.  On success,
+ * it then gets the new (and updated) document set, cleans up the data, and
+ * renders the templates with it.
+ */
+var submitText = function(text) {
+  Cards.setLoading(true);
+  MM.activeSession.textentries.post({
+    text: text,
+    type: 'text',
+    weight: 1.0
+  }, function onSuccess (textEntryResult) {
+    getDocuments();
+  }, function onFail (error) {
+    console.error('Error posting textEntry:', error);
+    Cards.setLoading(false);
+  });
+};
 
 /*
  * This gets the latest document set for the queries we've submitted.
@@ -64,91 +157,3 @@ var processRawCardData = function (card) {
 
   return card;
 };
-
-/*
- * This submits the text (aka `textEntry`) to the MindMeld API.  On success,
- * it then gets the new (and updated) document set, cleans up the data, and
- * renders the templates with it.
- */
-var submitText = function(text) {
-  Cards.setLoading(true);
-  MM.activeSession.textentries.post({
-    text: text,
-    type: 'text',
-    weight: 1.0
-  }, function onSuccess (textEntryResult) {
-    getDocuments();
-  }, function onFail (error) {
-    console.error('Error posting textEntry:', error);
-    Cards.setLoading(false);
-  });
-};
-
-/* Set up widget events */
-
-Microphone.on('start', function () {
-  console.log('Microphone started');
-});
-
-Microphone.on('stop', function () {
-  console.log('Microphone stopped');
-});
-
-/*
- * The error event has a String `error` field.  This is a short description
- * of the problem.  For a full list, please see
- * https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
- */
-Microphone.on('error', function (event) {
-  // Some errors are benign
-  if (event.error === 'aborted') {
-    console.log('Microphone aborted');
-  } else if (event.error === 'no-speech') {
-    console.log('Microphone did not receive any speech.');
-  } else {
-    console.error('Microphone error', event.error);
-  }
-});
-
-/*
- * This is the event handler when the microphone gets a result.  The result
- * object has a String `transcript` field, which contains the text of the
- * result, and a Boolean `final` field, which specifies whether the result is
- * the final result for this speech segment.  There will be many
- * `final == false` results, of increasing result (and accuracy).
- *
- * Here, we show the interim (`final == false`) results in the search field,
- * but greyed out to signify they are not final.  The final results are in
- * black.  We only submit the final results to MindMeld, since there are
- * many interim results that have minimal signal value.
- */
-Microphone.on('result', function(result) {
-  SearchInput.setText(result.transcript, result.final);
-  if (result.final) {
-    submitText(result.transcript);
-  }
-});
-
-/*
- * This is fired when the SearchInput is 'submitted', which means either
- * the magnifying glass search button is pressed, or the user presses
- * `<Return>` in the text box.
- */
-SearchInput.on('submitText', submitText);
-
-/* Initialize MindMeld */
-
-var config = {
-  appid: 'b7653579dda418eded308a4e491595057f337448'
-};
-
-MM.start(config, function onSuccess () {
-  console.log('MindMeld initialized successfully.');
-
-  // Pass in DOM (not jQuery) elements to these initializers.
-  Microphone.initialize($('.mindmeld-microphone')[0]);
-  SearchInput.initialize($('.mindmeld-search')[0]);
-
-}, function onFail (error) {
-  console.error('MindMeld failed to initialize:', error);
-});
