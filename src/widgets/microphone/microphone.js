bd5bee65ecef7029a706ef11b7b86c148123f3ab
J.J. Jackson <jj@expectlabs.com>
2014-12-18 14:29:26 -0800

    Disable to volume monitor in VN and mic widget to prevent “audio-capture” errors

--- src/widgets/microphone/microphone.js
+++ src/widgets/microphone/microphone.js
@@ -46,7 +46,11 @@
     }
 
     initMMListener();
-    initVolumeMonitor();
+
+    /* The volume monitor started causing "audio-capture" errors from webkitSpeechRecognition between
+     * Chrome 40.0.2214.38 beta (64-bit) and 40.0.2214.45 beta (64-bit). We are disabling it for now.
+     */
+    //initVolumeMonitor();
     initClickHandlers();
     initUIHandlers();
     MindMeldMicrophone.emit('init');
