cd1ff09f6e08f3903b7703e6a982e8038e0349f5
James Gill <jgill@expectlabs.com>
2015-01-15 16:37:33 -0800

    Initial work towards circle archiving in s3.

---
+++ deployScripts/deploy_archive.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+set -x # use + to only "echo" results on console (ie turn debugging off)
+set -e # exit if there are any errors at all (perfect build or none at all).
+
+SCRIPT_DIR=`dirname $0`
+ROOT_DIR=`dirname "$SCRIPT_DIR"`
+ARCHIVE_VERSION=$1
+
+if [ -z "$ARCHIVE_VERSION" ]; then
+  echo "Required to submit an archive version to deploy."
+  exit 1
+fi
+
+AWS_PROFILE=$2
+if [ -n "$AWS_PROFILE" ]; then
+  PROFILE="--profile $AWS_PROFILE"
+fi
+
+ZIP_FILE_NAME=mindmeld-js-sdk-${ARCHIVE_VERSION}.zip
+#aws s3 cp "$ROOT_DIR/archive/$ZIP_FILE_NAME" "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
+JS_FILE_NAME=mindmeld-${ARCHIVE_VERSION}.js
+aws $PROFILE s3 cp "$ROOT_DIR/archive/$JS_FILE_NAME" "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
