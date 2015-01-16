0fb07db09b8f5ac7d5a5264ce7dfb938ac7a0da8
James Gill <jgill@expectlabs.com>
2015-01-16 09:16:42 -0800

    Deply all archived files to s3.

--- deployScripts/deploy_archive.sh
+++ deployScripts/deploy_archive.sh
@@ -4,8 +4,8 @@ set -e # exit if there are any errors at all (perfect build or none at all).
 
 SCRIPT_DIR=`dirname $0`
 ROOT_DIR=`dirname "$SCRIPT_DIR"`
-ARCHIVE_VERSION=$1
 
+ARCHIVE_VERSION=$1
 if [ -z "$ARCHIVE_VERSION" ]; then
   echo "Required to submit an archive version to deploy."
   exit 1
@@ -17,6 +17,10 @@ if [ -n "$AWS_PROFILE" ]; then
 fi
 
 ZIP_FILE_NAME=mindmeld-js-sdk-${ARCHIVE_VERSION}.zip
-#aws s3 cp "$ROOT_DIR/archive/$ZIP_FILE_NAME" "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
 JS_FILE_NAME=mindmeld-${ARCHIVE_VERSION}.js
-aws $PROFILE s3 cp "$ROOT_DIR/archive/$JS_FILE_NAME" "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
+JS_MIN_FILE_NAME=mindmeld-${ARCHIVE_VERSION}.min.js
+aws $PROFILE s3 cp \
+  "$ROOT_DIR/archive/$JS_FILE_NAME" \
+  "$ROOT_DIR/archive/$JS_MIN_FILE_NAME" \
+  "$ROOT_DIR/archive/$ZIP_FILE_NAME" \
+  "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
