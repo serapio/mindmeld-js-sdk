8e2a8db7757dd80edf2b4f2dcdf75e25488e4ce8
James Gill <jgill@expectlabs.com>
2015-02-02 12:02:06 -0500

    Use only aws-cli for s3 commands, ditch s3cmd.

--- deployScripts/deploy_archive.sh
+++ deployScripts/deploy_archive.sh
@@ -2,6 +2,9 @@
 set -x # use + to only "echo" results on console (ie turn debugging off)
 set -e # exit if there are any errors at all (perfect build or none at all).
 
+# USAGE:
+# bash deployScripts/deploy_archive.sh ARCHIVE_VERSION [AWS_PROFILE]
+
 SCRIPT_DIR=`dirname $0`
 ROOT_DIR=`dirname "$SCRIPT_DIR"`
 
@@ -15,12 +18,12 @@ AWS_PROFILE=$2
 if [ -n "$AWS_PROFILE" ]; then
   PROFILE="--profile $AWS_PROFILE"
 fi
+S3CMD="aws $PROFILE s3"
 
 ZIP_FILE_NAME=mindmeld-js-sdk-${ARCHIVE_VERSION}.zip
 JS_FILE_NAME=mindmeld-${ARCHIVE_VERSION}.js
 JS_MIN_FILE_NAME=mindmeld-${ARCHIVE_VERSION}.min.js
 for file in $JS_FILE_NAME $JS_MIN_FILE_NAME $ZIP_FILE_NAME; do
-  aws $PROFILE s3 cp "$ROOT_DIR/archive/$file" \
+  $S3CMD cp "$ROOT_DIR/archive/$file" \
     "s3://elabs-archive/js-sdk/$ARCHIVE_VERSION/"
 done
-
