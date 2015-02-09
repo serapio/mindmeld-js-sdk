7fcaa7de60851497feaa70b6b85209f8914252dd
James Gill <jgill@expectlabs.com>
2015-02-09 09:56:18 -0500

    Version bump 2.9.3.

--- src/sdk/main.js
+++ src/sdk/main.js
@@ -18,7 +18,7 @@ var MM = ( function (window, ajax, Faye) {
      * @private
      */
     Object.defineProperty(MM, 'version', {
-        value: '2.9.2',
+        value: '2.9.3',
         writable: false
     });
 
